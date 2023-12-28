717a3d59a3b72570c8a7d24c1c8992cb7952a924
ACER <ACER@DESKTOP-QH5VR0V>
2023-12-28 18:42:13 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application") version "8.0.2" apply false
+
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+buildscript {
+    extra.apply {
+        set("nav_version", "2.5.3")
+        set("room_version", "2.5.2")
+    }
+}
